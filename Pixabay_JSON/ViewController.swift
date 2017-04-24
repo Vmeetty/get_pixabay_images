4505945cbcae033c62a3e92dba7187a1e9514684
Vlad Chuvashov <vladchuvashov@gmail.com>
2017-04-24 13:35:06 +0300

    Initial Commit

---
+++ Pixabay_JSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pixabay_JSON
+//
+//  Created by vlad on 24.04.17.
+//  Copyright © 2017 vladCh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
